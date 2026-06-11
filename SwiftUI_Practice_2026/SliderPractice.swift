b3da602a2a9e82b4ce1c144830b82da204a4d89f
heopill <heopill@naver.com>
2026-06-11 20:29:39 +0900

    [Feat] Slider 예제 추가

---
+++ SwiftUI_Practice_2026/SliderPractice.swift
@@ -0,0 +1,22 @@
+//
+//  SliderPractice.swift
+//  SwiftUI_Practice_2026
+//
+//  Created by 허성필 on 6/11/26.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    @State var grade: Double = 0
+    
+    var body: some View {
+        Slider(value: $grade, in: 0...100, step: 1)
+        Text("Your grade is \(grade)")
+        
+    }
+}
+
+#Preview {
+    SliderView()
+}
